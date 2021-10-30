707343f7f12803feead2e6e9b8cfd67bd8ca9412
Insality <insality@gmail.com>
2021-10-30 09:45:58 +0300

    Remove neighbors from cell's entities list

--- game/utils.lua
+++ game/utils.lua
@@ -1,6 +1,7 @@
 local M = {}
 
-local neighbors = {{0, 0}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}}
+-- local neighbors = {{0, 0}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}}
+local neighbors = {{0, 0}, {1, 1}, {1, 0}, {1, -1}, {0, -1}}
 local cell_size = 21
 local octotree = {}
 
@@ -47,12 +48,9 @@ end
 function M.octotree_add(entity)
     local x = math.floor(entity.position.x / cell_size)
     local y = math.floor(entity.position.y / cell_size)
-    for index = 1, #neighbors do
-        local n = neighbors[index]
-        local key = octotree_key(x + n[1], y + n[2])
-        check_octotree_cell(key)
-        table.insert(octotree[key], entity)
-    end
+    local key = octotree_key(x, y)
+    check_octotree_cell(key)
+    table.insert(octotree[key], entity)
 end
 
 
@@ -68,35 +66,30 @@ function M.octotree_update(entity)
         return
     end
 
-    for index = 1, #neighbors do
-        local n = neighbors[index]
-        local test_key = octotree_key(x_prev + n[1], y_prev + n[2])
-        for i = #octotree[test_key], 1, -1 do
-            if octotree[test_key][i] == entity then
-                table.remove(octotree[test_key], i)
-            end
+    for i = #octotree[key_prev], 1, -1 do
+        if octotree[key_prev][i] == entity then
+            table.remove(octotree[key_prev], i)
         end
     end
 
-    for index = 1, #neighbors do
-        local n = neighbors[index]
-        local test_key = octotree_key(x + n[1], y + n[2])
-        check_octotree_cell(test_key)
-        table.insert(octotree[test_key], entity)
-    end
+    check_octotree_cell(key)
+    table.insert(octotree[key], entity)
 end
 
 
 function M.octotree_foreach(entity, callback)
     local x = math.floor(entity.position.x / cell_size)
     local y = math.floor(entity.position.y / cell_size)
-    local key = octotree_key(x, y)
-    check_octotree_cell(key)
 
-    local entities = octotree[key]
-    for index = 1, #entities do
-        if entity ~= entities[index] then
-            callback(entities[index])
+    for i = 1, #neighbors do
+        local n = neighbors[i]
+        local key = octotree_key(x + n[1], y + n[2])
+        check_octotree_cell(key)
+        local entities = octotree[key]
+        for index = 1, #entities do
+            if entity ~= entities[index] then
+                callback(entities[index])
+            end
         end
     end
 end
